b0967ea8839a42b2ba062e6c319b2128a1c8166f
Neadro <gysev.sergei04@gmail.com>
2025-02-16 17:50:16 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,35 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   public User getUserByMS(String model, int series){
+      return sessionFactory.getCurrentSession().createQuery("select a from User a join a.car b where b.model=:model and b.series=:series", User.class)
+              .setParameter("model", model).setParameter("series", series).getSingleResult();
+   }
+
+
+}
